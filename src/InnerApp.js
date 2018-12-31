90488c158a94e9ac1e0f0d891c2bc2fcf5b8ba05
unknown <devethancraig@gmail.com>
2018-12-30 21:10:43 -0700

    Routing Extended - Still Not Working

--- src/InnerApp.js
+++ src/InnerApp.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import {View} from 'react-native';
 import {connect} from 'react-redux';
+import Navigation from '../actions/Navigation';
 import {Root} from 'native-base';
 import {StyleSheet} from 'react-native';
 
-import {LoginStack} from './Router';
+import {LoginStack, InnerStack} from './Router';
 
 class InnerApp extends React.Component {
+    state = {
+        bool: false
+    }
     render() {
+        let NavigationStack = bool ? (
+            <Root>
+                <InnerStack 
+                    ref={navigatorRef => {
+                        Navigation.setTopLevelNavigator(navigatorRef);
+                    }}
+                />
+            </Root>
+        ) : (
+            <LoginStack />
+        );
         return(
-            <View>
-                <LoginStack />
-            </View>
+            NavigationStack
         );
     }
 }
