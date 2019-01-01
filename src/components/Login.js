0dc3a957c762ff86e87d4de3d6b050637bdd867a
unknown <devethancraig@gmail.com>
2018-12-31 22:04:40 -0700

    Login && Register Styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,57 @@
 import React, {Component} from 'react';
-import {Button, TextInput, Text, View} from 'react-native';
+import {Button, StyleSheet, TextInput, Text, View} from 'react-native';
 
 class Login extends Component {
     render() {
         return(
             <View style={{flex: 1}}>
-                <Text>Login</Text>
-                <Text>Login to your Gym Assistant account here.</Text>
-                <TextInput 
-                    placeholder='Email'
-                />
-                <TextInput
-                    placeholder='Password'
-                />
-                <Button title='Login' />
+                <Text style={styles.header}>Login</Text>
+                <Text style={styles.bodyText}>Login to your <Text style={styles.appName}>Gym Assistant</Text> account here.</Text>
+                    <View style={styles.container}>
+                        <TextInput 
+                            placeholder='Email'
+                            style={styles.itemContainer}
+                        />
+                        <TextInput
+                            placeholder='Password'
+                            style={styles.itemContainer}
+                        />
+                    </View>
+                    <Button title='Login' />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    itemContainer: {
+        width: '50%',
+        marginTop: 25,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da'
+    },
+    header: {
+        fontSize: 28,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 80
+    },
+    appName: {
+        color: '#ff8c1a',
+        fontWeight: 'bold',
+    },
+    bodyText: {
+        fontSize: 18,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 40
+    }  
+});
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,30 +1,65 @@
 import React, {Component} from 'react';
-import {Button, TextInput, Text, View} from 'react-native';
+import {Button, StyleSheet, TextInput, Text, View} from 'react-native';
 
 class Register extends Component {
     render() {
         return(
             <View style={{flex: 1}}>
-                <Text>Create Your New Account</Text>
-                <Text>Create your own Gym Assistant account here.</Text>
-                <TextInput
-                    placeholder='Name'
-                />
-                <TextInput
-                    placeholder='Email'
-                />
-                <TextInput
-                    placeholder='Password'
-                />
-                <TextInput
-                    placeholder='Confirm Password'
-                />
-                <Button 
-                    title='Create Account'
-                />
+                <Text style={styles.header}>Create Your New Account</Text>
+                <Text style={styles.bodyText}>Create your own <Text style={styles.appName}>Gym Assistant</Text> account here.</Text>
+                <View style={styles.container}>
+                    <TextInput
+                        placeholder='Name'
+                        style={styles.itemContainer}
+                    />
+                    <TextInput
+                        placeholder='Email'
+                        style={styles.itemContainer}
+                    />
+                    <TextInput
+                        placeholder='Password'
+                        style={styles.itemContainer}
+                    />
+                    <TextInput
+                        placeholder='Confirm Password'
+                        style={styles.itemContainer}
+                    />
+                </View>
+                <Button title='Create Account' />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    itemContainer: {
+        width: '50%',
+        marginTop: 25,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da'
+    },
+    header: {
+        fontSize: 28,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 80
+    },
+    appName: {
+        color: '#ff8c1a',
+        fontWeight: 'bold',
+    },
+    bodyText: {
+        fontSize: 18,
+        color: 'black',
+        textAlign: 'center',
+        marginTop: 40
+    }  
+});
 
 export default Register;
